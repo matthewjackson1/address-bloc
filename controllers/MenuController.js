806089d81b2d829acfdfa7185b39f844688252a6
Matthew Jackson <Matthew@Matthew-Jacksons-MacBook-Pro.local>
2018-03-11 22:20:58 +0000

    listing-searching complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,7 @@ module.exports = class MenuController {
 	        message: "Please choose from an option below: ",
 	        choices: [
 	          "Add new contact",
+            "View all contacts",
 	          "Get the current date",
 	          "Exit"
 	        ]
@@ -42,11 +43,21 @@ module.exports = class MenuController {
           }
         }
       ];
+
+    this.searchQuestions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Name of contact to search - ",
+        validate(val){
+          return val !== "";
+        }
+      }
+    ];
     }
     
 
     main(){
-      console.log(`Welcome to AddressBloc! You have ${this.book.contacts.length} entries.`);
       inquirer.prompt(this.mainMenuQuestions).then((response) => {
         switch(response.mainMenuChoice){
           case "Add new contact":
@@ -55,6 +66,12 @@ module.exports = class MenuController {
           case "Get the current date":
           	this.getDate();
           	break;
+          case "View all contacts";
+            this.getContacts();
+            break;
+          case "Search for a contact":
+            this.search();
+            break;
           case "Exit":
             this.exit();
           default:
@@ -98,6 +115,89 @@ module.exports = class MenuController {
     remindMe(){
       return "Learning is a life-long pursuit";
     }
+
+    getContacts(){
+      this.clear();
+
+      this.book.getContacts().then((contacts) => {
+        for (let contact of contacts) {
+          console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`
+          );
+        }
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+    search(){
+      inquirer.prompt(this.book.searchQuestions).then((target) => {
+       this.book.search(target.name).then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+         }
+
+        });
+     }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+    showContact(contact){
+      this._printContact(contact);
+      inquirer.prompt(this.book.showContactQuestions).then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    }).catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+    }
+
+    _printContact(contact){
+      console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+      );
+    }
+
+    delete(contact){
+      inquirer.prompt(this.book.confirmDeleteQuestions).then((answer) => {
+        if(answer.confirmation){
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      }).catch((err) {
+        console.log(err);
+        this.main();
+      });
+    }
+
 
     exit(){
       console.log("Thanks for using AddressBloc!");
--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -36,6 +36,203 @@ describe('ContactController', () => {
       });
 
   });
+
+
+  describe("#getContacts()", () => {
+
+      it("should return an empty array when no contacts are available", (done) => {
+        book.getContacts().then((contacts) => {
+          expect(contacts.length).toBe(0);
+          done();
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+      it("should return an array of contacts when contacts are available", (done) => {
+        book.addContact("Alice", "001-101-1010", "alice@example.com").then(() => {
+          book.getContacts().then((contacts) => {
+            expect(contacts.length).toBe(1);
+            done();
+          });
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+    });
+
+describe("search methods", () => {
+
+      const zelda = ["Zelda Smith", "000-100-111", "zelda@nintendo.com"];
+      const snake = ["Solid Snake", "100-100-100", "snake@konami.com"];
+      const magus = ["Magus Johnson", "101-010-101", "magus@squaresoft.com"];
+      const alloy = ["Alloy Rodriguez", "111-111-111", "allow@guerrilla-games.com"];
+
+
+  describe("#iterativeSearch()", () => {
+      it("should return null when called with an empty array", (done) => {
+        expect(book.iterativeSearch([], "Alloy")).toBeNull();
+      });
+
+      it("should return null when contact is not found", (done) => {
+        book.addContact(...zelda).then(() => {
+          book.getContacts().then((contacts) => {
+            expect(book.iterativeSearch(contacts, "Alloy Rodriguez")).toBeNull();
+            done();
+          });
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+      it("should return the contact if found", (done) => {
+        book.addContact(...alloy).then(() => {
+          book.addContact(...magus).then(() => {
+            book.getContacts().then((contacts) => {
+              let contact = book.iterativeSearch(contacts, "Magus Johnson");
+              expect(contact.name).toBe("Magus Johnson");
+              expect(contact.phone).toBe("101-010-101");
+              expect(contact.email).toBe("magus@squaresoft.com");
+             done();
+            });
+          });
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+    });
+
+  describe("#binarySearch()", () => {
+
+        function sort(contacts){
+         return contacts.sort((a, b) => {
+           if(a.name > b.name) return 1;
+           else if(a.name < b.name) return -1;
+           else return 0;
+         });
+        }
+
+        it("should return null when called with an empty array", () => {
+          expect(book.binarySearch([], "Alloy Rodriguez")).toBeNull();
+        });
+
+        it("should return null when contact is not found", (done) => {
+          book.addContact(...zelda).then(() => {
+            book.getContacts().then((contacts) => {
+              expect(book.binarySearch(sort(contacts), "Alloy Rodriguez")).toBeNull();
+              done();
+            });
+          }).catch((err) => {
+            console.log(err);
+            done();
+          });
+        });
+
+        it("should return the contact if found", (done) => {
+          book.addContact(...alloy).then(() => {
+            book.addContact(...magus).then(() => {
+              book.addContact(...zelda).then(() => {
+                book.addContact(...snake).then(() => {
+                  book.getContacts().then((contacts) => {
+                    let contact = book.binarySearch(sort(contacts), "Magus Johnson");
+                    expect(contact.name).toBe("Magus Johnson");
+                    expect(contact.phone).toBe("101-010-101");
+                    expect(contact.email).toBe("magus@squaresoft.com");
+                    done();
+                  });
+                });
+              });
+            });
+          }).catch((err) => {
+            console.log(err);
+            done();
+          });
+        });
+
+      });
+
+    });
+
+    
+    describe("#search()", () => {
+      it("should return null when a contact was not found", (done) => {
+        book.addContact(...zelda).then(() => {
+          book.search("Solid Snake").then((contact) => {
+            expect(contact).toBeNull();
+            done();
+          });
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+      it("should return the contact when found", (done) => {
+        book.addContact(...snake).then(() => {
+          book.search("Solid Snake").then((contact) => {
+            expect(contact).not.toBeNull();
+            expect(contact.name).toBe("Solid Snake");
+            expect(contact.phone).toBe("100-100-100");
+            expect(contact.email).toBe("snake@konami.com");
+            done();
+          });
+        }).catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+    });
+
+
+    describe("#delete()", () => {
+
+      it("should not remove any contacts that do not match the ID passed", (done) => {
+        book.addContact("Rick Deckard", "000-000-000", "null@null.com").then(() => {
+          book.getContacts().then((contacts) => {
+            expect(contacts[0].name).toBe("Rick Deckard");
+            expect(contacts.length).toBe(1);
+            book.delete(99).then(() => {
+              book.getContacts().then((contacts) => {
+                expect(contacts.length).toBe(1);
+                done();
+              });
+            });
+          }).catch((err) => {
+            console.log(err);
+            done();
+          });
+        });
+      });
+
+      it("should remove the contact that matches the ID passed", (done) => {
+
+        book.addContact("Rick Deckard", "000-000-000", "null@null.com").then((contact) => {
+          book.getContacts().then((contacts) => {
+            expect(contacts[0].name).toBe("Rick Deckard");
+            expect(contacts.length).toBe(1);
+            book.delete(contact.id).then(() => {
+              book.getContacts().then((contacts) => {
+                expect(contacts.length).toBe(0);
+                done();
+              });
+            });
+          }).catch((err) => {
+            console.log(err);
+            done();
+          });
+        });
+
+      });
+
+    });
+    
 });
 
 
